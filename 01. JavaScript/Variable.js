b4dbd95267afe82f371960d5f0e429a59960bfb8
marcelo nieto <marcechelo25@hotmail.com>
2018-04-18 12:29:14 -0500

    variables

--- 01. JavaScript/Variable.js
+++ 01. JavaScript/Variable.js
@@ -1,1 +1,15 @@
-console.log('hola mundo');
+//console.log('hola mundo');
+var nombre = 'marcelo';
+var edad = 22;
+var peso = 305.1;
+var casaedo = false;
+var fecha = new Date();
+var noExisto = null;
+var noEstoyDefinido = undefined;
+
+if (noEstoyDefinido) {
+    console.log('verdadero');
+} else {
+    console.log('falso')
+}
+
