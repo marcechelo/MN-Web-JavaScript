0d909b9d95ab8a9690aaf2bd0d27010dcdffebd9
marcelo nieto <marcechelo25@hotmail.com>
2018-04-18 12:44:31 -0500

    Objetos

--- 01. JavaScript/Variable.js
+++ 01. JavaScript/Variable.js
@@ -6,10 +6,32 @@ var casaedo = false;
 var fecha = new Date();
 var noExisto = null;
 var noEstoyDefinido = undefined;
+var mutar;
 
-if (noEstoyDefinido) {
+//Json, se llaman Objetos
+var usuario = {
+    nombre: 'Marcelo',
+    apellido: 'Nieto',
+    cedula: '871923',
+    edad: 22,
+    //imprimir
+};
+
+console.log(usuario);
+console.log(usuario.nombre);
+delete usuario.edad;
+console.log(usuario);
+usuario.fechaNaciemientoa = new Date();
+console.log(usuario);
+usuario.mascotas = {};
+usuario.mascotas.nombre = 'pepito';
+console.log(usuario);
+
+
+/*if (-1) {
     console.log('verdadero');
 } else {
     console.log('falso')
-}
+}*/
+
 
