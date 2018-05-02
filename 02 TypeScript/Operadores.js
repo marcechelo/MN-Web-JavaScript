815ab7bf3d188da0470ebcfd12bd2ad1fd24c16b
marcelo nieto <marcechelo25@hotmail.com>
2018-05-02 12:35:53 -0500

    Operadores

--- 02 TypeScript/Operadores.js
+++ 02 TypeScript/Operadores.js
@@ -1,21 +1,35 @@
 var arreglonumeros = [1, 2, 3, 4, 5];
 //let arregloUsuario: Array<UsuarioArreglo>= [
 var arregloUsuarios = [
-    { nombre: 'adrian',
-        edad: 28 },
-    { nombre: 'gabriela',
-        edad: 10 },
-    { nombre: 'felipe',
-        edad: 68 },
-    { nombre: 'wendy',
-        edad: 32 },
-    { nombre: 'roberto',
-        edad: 18 },
+    {
+        nombre: 'adrian',
+        edad: 28
+    },
+    {
+        nombre: 'gabriela',
+        edad: 10
+    },
+    {
+        nombre: 'felipe',
+        edad: 68
+    },
+    {
+        nombre: 'wendy',
+        edad: 32
+    },
+    {
+        nombre: 'roberto',
+        edad: 18
+    },
 ];
 //fat arrow function
-var sumarDosNumero = function (numeroUno, numeroDos) { return numeroUno + numeroDos; };
+var sumarDosNumero = function (numeroUno, numeroDos) {
+    return numeroUno + numeroDos;
+};
 var sumarDosNumeroV2 = function (numeroUno, numeroDos) { return numeroUno + numeroDos; };
-var potenciaDeUnNumero = function (numero) { return numero * numero; };
+var potenciaDeUnNumero = function (numero) {
+    return numero * numero;
+};
 var potenciaDeUnNumeroV2 = function (numero) { return numero * numero; };
 //Operador ForEach
 var sumaTotal = 0;
@@ -34,13 +48,22 @@ var resultadoSumaConReduce = arreglonumeros.reduceRight(function (valorAcumulado
     return valorAcumuladoActual - valorActualArreglo;
 }, 20);
 //console.log(resultadoSumaConReduce);
-var sumaEdadesDeUsuarios = arregloUsuarios.reduce(function (acumuladoEdad, usuario) {
-    return acumuladoEdad + usuario.edad;
-}, 0);
-console.log(sumaEdadesDeUsuarios);
+var sumaEdadesDeUsuarios = arregloUsuarios.reduce(function (acumuladoEdad, usuario) { return acumuladoEdad + usuario.edad; }, 0);
+//console.log(sumaEdadesDeUsuarios);
 //Operador Map
 var nuevoArregloDeUsuarios = arregloUsuarios.map(function (usuario) {
-    usuario.deuda = 0;
+    usuario.becado = false;
     return usuario;
-});
-console.log(nuevoArregloDeUsuarios);
+}).map(function (usuario) {
+    usuario.deuda = calcularDeuda(usuario.edad);
+    return usuario;
+    //Operador filter
+})
+    .filter(function (usuario) { return usuario.deuda < 50; });
+//Operadores some y every
+//.some((usuario:UsuarioArreglo)=>usuario.edad<18);
+console.log('nuevo arreglo de usuarios' + '\n', nuevoArregloDeUsuarios);
+function calcularDeuda(edadUsuario) {
+    var totalEdad = arregloUsuarios.reduce(function (total, usuario) { return total + usuario.edad; }, 0);
+    return totalEdad * (edadUsuario / 100);
+}
--- 02 TypeScript/Operadores.ts
+++ 02 TypeScript/Operadores.ts
@@ -1,64 +1,90 @@
-let arreglonumeros = [1,2,3,4,5];
+let arreglonumeros = [1, 2, 3, 4, 5];
 
 //let arregloUsuario: Array<UsuarioArreglo>= [
-let arregloUsuarios:UsuarioArreglo[]=[
-    {nombre:'adrian',
-        edad:28},
-    {nombre:'gabriela',
-        edad:10},
-    {nombre:'felipe',
-        edad:68},
-    {nombre:'wendy',
-        edad:32},
-    {nombre:'roberto',
-        edad:18},
+let arregloUsuarios: UsuarioArreglo[] = [
+    {
+        nombre: 'adrian',
+        edad: 28
+    },
+    {
+        nombre: 'gabriela',
+        edad: 10
+    },
+    {
+        nombre: 'felipe',
+        edad: 68
+    },
+    {
+        nombre: 'wendy',
+        edad: 32
+    },
+    {
+        nombre: 'roberto',
+        edad: 18
+    },
 ];
 
 //Interfaz
 
 interface UsuarioArreglo {
-    nombre:String,
-    edad:number,
-    deuda?:number
+    nombre: String,
+    edad: number,
+    deuda?: number,
+    becado?: boolean
 }
 
 //fat arrow function
-let sumarDosNumero=(numeroUno:number,numeroDos:number):number=>{return numeroUno+numeroDos};
-let sumarDosNumeroV2=(numeroUno:number,numeroDos:number):number=> numeroUno+numeroDos;
+let sumarDosNumero = (numeroUno: number, numeroDos: number): number => {
+    return numeroUno + numeroDos
+};
+let sumarDosNumeroV2 = (numeroUno: number, numeroDos: number): number => numeroUno + numeroDos;
 
-let potenciaDeUnNumero = numero =>{return numero*numero};
-let potenciaDeUnNumeroV2 = numero => numero*numero;
+let potenciaDeUnNumero = numero => {
+    return numero * numero
+};
+let potenciaDeUnNumeroV2 = numero => numero * numero;
 
 //Operador ForEach
 let sumaTotal = 0;
-let resultadoForEach = arreglonumeros.forEach((valorActual,indiceActual,arreglo)=>{
-    sumaTotal = sumaTotal + valorActual;
-    //console.log(valorActual);
-    //console.log(indiceActual);
-    //console.log(arreglo);
+let resultadoForEach = arreglonumeros.forEach((valorActual, indiceActual, arreglo) => {
+        sumaTotal = sumaTotal + valorActual;
+        //console.log(valorActual);
+        //console.log(indiceActual);
+        //console.log(arreglo);
     }
 );
 //console.log(resultadoForEach);
 //console.log(sumaTotal);
 
 //Operador Reduce
-let resultadoSumaConReduce = arreglonumeros.reduceRight((valorAcumuladoActual,valorActualArreglo)=>{
-    //console.log('valor acumulado actual', valorAcumuladoActual);
-    //console.log('valor actual del arreglo',valorActualArreglo);
-    return valorAcumuladoActual-valorActualArreglo},
+let resultadoSumaConReduce = arreglonumeros.reduceRight((valorAcumuladoActual, valorActualArreglo) => {
+        //console.log('valor acumulado actual', valorAcumuladoActual);
+        //console.log('valor actual del arreglo',valorActualArreglo);
+        return valorAcumuladoActual - valorActualArreglo
+    },
     20);
 //console.log(resultadoSumaConReduce);
 
-let sumaEdadesDeUsuarios = arregloUsuarios.reduce((acumuladoEdad:number,usuario:UsuarioArreglo)=>{
-    return acumuladoEdad+usuario.edad;
-},0
-);
-console.log(sumaEdadesDeUsuarios);
+let sumaEdadesDeUsuarios = arregloUsuarios.reduce((acumuladoEdad: number, usuario: UsuarioArreglo) => acumuladoEdad + usuario.edad, 0);
+//console.log(sumaEdadesDeUsuarios);
 
 //Operador Map
-let nuevoArregloDeUsuarios = arregloUsuarios.map((usuario:UsuarioArreglo)=> {
-        usuario.deuda = 0;
+let nuevoArregloDeUsuarios = arregloUsuarios.map((usuario: UsuarioArreglo) => {
+        usuario.becado = false;
         return usuario;
     }
-);
-console.log(nuevoArregloDeUsuarios);
+).map((usuario: UsuarioArreglo) => {
+    usuario.deuda = calcularDeuda(usuario.edad);
+    return usuario;
+//Operador filter
+})
+    .filter((usuario: UsuarioArreglo) => usuario.deuda < 50);
+//Operadores some y every
+//.some((usuario:UsuarioArreglo)=>usuario.edad<18);
+
+console.log('nuevo arreglo de usuarios' + '\n', nuevoArregloDeUsuarios);
+
+function calcularDeuda(edadUsuario: number) {
+    const totalEdad = arregloUsuarios.reduce((total, usuario: UsuarioArreglo) => total + usuario.edad, 0);
+    return totalEdad * (edadUsuario / 100);
+}
